ca8ec7a2fcd316a92f1e808d72214ee1db034863
hecatris <74726881+hecatris@users.noreply.github.com>
2021-12-12 10:30:31 +0100

    made some adjustments

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <template>
   <div id="app">
-    <BarChart />
-    <TauxTelechargement />
-    <Affinites />
+    <h1>Les pratiques d’écoute de musique en ligne</h1>
+    <div class="graph_container">
+      <h2>Nombres découtes et part du téléchargement dans celles-ci</h2>
+      <BarChart />
+    </div>
+    <div class="graph_container">
+      <h2>Taux de telechargement par style écouté</h2>
+      <TauxTelechargement />
+    </div>
+    <div class="graph_container">
+      <h2>Taux d'affinités entres styles</h2>
+      <Affinites />
+    </div>
+    <footer>
+      affichage basé sur un sondage réalisé par Hadopi sur un échantillion de
+      2200 français en octobre 2020.
+      <a
+        href="https://www.data.gouv.fr/fr/datasets/les-pratiques-decoute-de-musique-en-ligne-octobre-2020"
+      >
+        Source
+      </a>
+    </footer>
   </div>
 </template>
 
@@ -29,5 +48,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  padding-left: 10px;
+  padding-right: 10px;
+}
+
+h1 {
+  padding-bottom: 6vh;
+}
+
+footer {
+  text-align: start;
+  font-size: smaller;
+}
+
+.graph_container {
+  padding-bottom: 5vh;
 }
 </style>
